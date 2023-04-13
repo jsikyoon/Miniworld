246da9f352f83f96b814cc3a2bddf023074dade4
Jaesik Yoon <jaesik.yoon.kr@gmail.com>
2023-04-13 18:59:03 +0900

    modifying generation code

--- miniworld_test/generate_data_for_stmem.py
+++ miniworld_test/generate_data_for_stmem.py
@@ -21,12 +21,12 @@ import skvideo.io
 from einops import rearrange
 import matplotlib.pyplot as plt
 
-def generate_video(image, top_camera, agent_pos, obs_steps, dir, idx):
+def generate_video(image, dir, idx):
 
     total_steps, h, w, c = image.shape  # _, 84(h=y), 84(w=x)
 
     # make video
-    video = np.concatenate((image, top_camera), axis=2)
+    video = image
     fps = '4'
     crf = '17'
     vid_out = skvideo.io.FFmpegWriter(f'{dir}/video_{idx}.mp4',
@@ -39,75 +39,51 @@ def generate_video(image, top_camera, agent_pos, obs_steps, dir, idx):
         vid_out.writeFrame(frame)
     vid_out.close()
 
-    # save traj scenes
-    n_r = int(np.sqrt(total_steps))
-    n_c = total_steps // n_r
-    n_vis = n_r * n_c
-    image_tosave = rearrange(image[:n_vis],
-                             '(n_r n_c) h w c -> (n_r h) (n_c w) c',
-                             n_r=n_r, n_c=n_c)
-    plt.imsave(f'{dir}/image_{idx}.png', image_tosave)
-
 
 # setting
-
-env_type = 'V0'
-env = gym.make(f"MiniWorld-Custom{env_type}-v0")
-policy_type = 'rotate'
-type_mapper = {
-    'random': [HeuristicPolicyRandom, 50, 100, 100, None],
-    # for a basic dataset
-    'mfmb': [HeuristicPolicyMFMB, 70, 100, 5000, None],
-    'rotate': [HeuristicPolicyRotate, 50, 80, 5000, None],
-    #'rotate': [HeuristicPolicyRotate, 23, 30, 5000, None],
-    'repeat': [HeuristicPolicyRepeat, 70, 100, 20000, None],
-}
-
-add_input = type_mapper[policy_type][4]
-if add_input is not None:
-    actor = type_mapper[policy_type][0](add_input)
-else:
-    actor = type_mapper[policy_type][0]()
-obs_steps = type_mapper[policy_type][1]
-total_steps = type_mapper[policy_type][2]
-total_num_ep = type_mapper[policy_type][3]
-assert total_steps > obs_steps
-data_dir = f'./datasets/offline_miniworld_{env_type}_{policy_type}'
+total_num_ep = 5000
+data_dir = f'./datasets/room_with_without_objects_stmem'
 Path(data_dir).mkdir(parents=True, exist_ok=True)
 data_type = {'train': int(total_num_ep * 0.9),
              'eval': int(total_num_ep * 0.1)}
-num_action = env.action_space.n
-
 
 # generate data
 data_type = {'train': int(total_num_ep * 0.9), 'eval': int(total_num_ep * 0.1)}
 env_with_objects = gym.make(f"MiniWorld-RoomObjectsSTMEM-v0")
 env_without_objects = gym.make(f"MiniWorld-RoomSTMEM-v0")
-# observations without objects
-obss_without_objects = []
-obs, info = env_without_objects.reset()
-obss_without_objects.append(obs)
-print(obs.keys());exit(1)
+# observations with objects
 for type, num_ep in data_type.items():
-
-    Path(f'{data_dir}/{type}').mkdir(parents=True, exist_ok=True)
-
+    if type == 'eval':
+        seed = data_type['train'] + num_ep
+    else:
+        seed = num_ep
     for j in range(num_ep):
+        dict = {}
+        # observations with objects
+        obss_with_objects, dirs_with_objects = [], []
+        obs, info = env_with_objects.reset(seed=seed)
+        obss_with_objects.append(obs['image'])
+        dirs_with_objects.append(round(obs['agent_dir']/(2*np.pi)*360 % 360))
+        for _ in range(23): # 360 / 15
+            obs, _, _, _, _ = env_with_objects.step(0) # turn left
+            obss_with_objects.append(obs['image'])
+            dirs_with_objects.append(round(obs['agent_dir']/(2*np.pi)*360 % 360))
+        # observations without objects
+        obss_without_objects, dirs_without_objects = [], []
+        obs, info = env_without_objects.reset(seed=seed)
+        obss_without_objects.append(obs['image'])
+        dirs_without_objects.append(round(obs['agent_dir']/(2*np.pi)*360 % 360))
+        for _ in range(23): # 360 / 15
+            obs, _, _, _, _ = env_without_objects.step(0) # turn left
+            obss_without_objects.append(obs['image'])
+            dirs_without_objects.append(round(obs['agent_dir']/(2*np.pi)*360 % 360))
 
-        # actor interact with env for total_steps
-
-        dict = actor.interaction(env, total_steps)
-
-        # save first 3 videos
-
+        dict['image'] = np.array(obss_with_objects + obss_without_objects)
+        dict['agent_dir'] = np.array(dirs_with_objects + dirs_without_objects)
+        Path(f'{data_dir}/{type}').mkdir(parents=True, exist_ok=True)
+        # save first 10 videos
         if type == 'train' and j < 10:
-
-            generate_video(dict['image'].copy().astype(np.uint8),
-                           dict['top_camera'].copy().astype(np.uint8),
-                           dict['agent_pos'].copy(),
-                           obs_steps, data_dir, j)
-
+            generate_video(dict['image'].copy().astype(np.uint8), data_dir, j)
+            exit(1)
         print(f'saving {type}/{j}-th episode in npz...')
-        # save only first image of top_camera
-        dict['top_camera'] = dict['top_camera'][0:1]
-        np.savez(f'{data_dir}/{type}/{j}_{total_steps}.npz', **dict)
+        np.savez(f'{data_dir}/{type}/{j}.npz', **dict)
